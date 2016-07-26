a8eacf691ffb3d26ae4a10da0072da1606afab87
Matthew Kory <matthewskory@gmail.com>
2016-07-26 10:43:21 -0400

    linked to mlab

--- node-js-playlist-master/node-js-playlist-master/controllers/todocontroller.js
+++ node-js-playlist-master/node-js-playlist-master/controllers/todocontroller.js
@@ -1,31 +1,61 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
+//connect to database
+mongoose.connect('mongodb://test:test@ds029725.mlab.com:29725/todo_app');
 
-var data = [{item: 'walk pet duck'},{item:'eat some vegetables'},{item:'punch a nun'}];
+//create schema 
+var todoSchema = new mongoose.Schema({
+	item: String
+});
+
+//create todo model - based on schema
+var Todo = mongoose.model('Todo', todoSchema);
+//test data for database -------------------------------------------------
+// var itemOne = Todo({item: "dig some holes in the woods"}).save(function(err){
+// 	if (err) throw err;
+// 	console.log('item saved');
+// });
+
+//dummy data for local------------------------------------------------------
+//var data = [{item: 'walk pet duck'},{item:'eat some vegetables'},{item:'punch a nun'}];
 
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
 module.exports = function(app){
 
-
 app.get('/todo', function(req, res){
-	res.render('todo', {todos: data});
+	//get data from mongo and pass to view
+	Todo.find({}, function(err, data){ //empty object gets all items, specify the item to get a single item returned
+		if (err) throw error;
+		res.render('todo', {todos: data});	
+	}) 
+	
 });
 
 
 app.post('/todo', urlencodedParser,	function(req, res){
-	data.push(req.body);
-	res.json(data);
+	//get data from the view and add it to mongodb
+	var newTodo = Todo(req.body).save(function(err, data){
+		if (err) throw err;
+		res.json(data);
+	})
 });
 
 // grab the item name out of the url using ajax delete request on todo-list.js 
 app.delete('/todo/:item', function(req, res){
-	//filter the data list using the attached function
-	data = data.filter(function(todo){
-		//checks the items in data and compares it to the req.params.item from the url. remove the one in the url.
-		return todo.item.replace(/ /g, '-') !== req.params.item;
+
+	Todo.find({item: req.params.item.replace(/\-/g, ' ')}).remove(function(err, data){
+		if (err) throw err;
+		res.json(data);
 	});
-	res.json(data);
-});
+	//commented out portions used in the static file --------------------------------------
+	// //filter the data list using the attached function
+	// data = data.filter(function(todo){
+	// 	//checks the items in data and compares it to the req.params.item from the url. remove the one in the url.
+	// 	return todo.item.replace(/ /g, '-') !== req.params.item;
+	});
+	// res.json(data);
+
 
 };
